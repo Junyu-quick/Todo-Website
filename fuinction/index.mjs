063340839182cdb939e8c98952ffc5024ca39e38
junyuquick <junyuquickwork@gmail.com>
2024-03-01 01:59:03 +0800

    created handler

---
+++ fuinction/index.mjs
@@ -0,0 +1,17 @@
+import {LambdaClient, GetAccountSettingsCommand} from '@aws-sdk/client-lambda';
+// Create client outside of handler to reuse
+const lambda = new LambdaClient();
+const express = require('express');
+const app = express();
+
+// Handler
+export const handler = async (event, context) => {
+  require('./startup/db.js')();
+  require('./startup/routes.js')(app);
+}
+
+
+// Use SDK client
+var getAccountSettings = function(){
+  return lambda.getAccountSettings().promise()
+}
